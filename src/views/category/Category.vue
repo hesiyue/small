1c727f9b76e582cf53f05c11d983b0f64bac0c8f
hesiyue <2679903081@qq.com>
2019-11-16 15:49:43 +0800

    '由于逗比操作，提交本地代码到github上时出现了些错误，导致写的category被以前的覆盖了，所以又重新写了次，都是因为github使用不熟练啊'

--- src/views/category/Category.vue
+++ src/views/category/Category.vue
@@ -1,18 +1,78 @@
 <template>
-    <div>
-        <h1>我是分类</h1>
-
+    <div id="category">
+        <nav-bar class="nav-bar"><div slot="center">商品分类</div></nav-bar>
+        <category-menu @showGoods="showGoods"></category-menu>
+        <scroll class="content">
+            <goods-list :goods="goods.list"></goods-list>
+        </scroll>
     </div>
 </template>
 
 <script>
-    import BackTop from "../../components/content/backTop/BackTop";
+    import NavBar from "components/common/navbar/NavBar";
+    import CategoryMenu from "./childComps/CategoryMenu";
+    import GoodsList from "components/content/goods/GoodsList";
+    import Scroll from "../../components/common/scroll/Scroll";
+    import {getDetailKind} from "../../network/category";
+
     export default {
         name: "Category",
-        components: {}
+        components: {Scroll, NavBar,CategoryMenu,GoodsList},
+        data(){
+            return {
+                goods:{list:[]}
+            }
+        },
+        methods: {
+            showGoods(index){
+                let kind = null
+                switch (index) {
+                    case 0: kind = 'clothes'
+                            break
+                    case 1: kind = 'pants'
+                        break
+                    case 2: kind = 'dress'
+                        break
+                    case 3: kind = 'suit'
+                        break
+                    case 4: kind = 'bras'
+                        break
+                    case 5: kind = 'shoes'
+                        break
+                    case 6: kind = 'skincare'
+                        break
+                    case 7: kind = 'makeup'
+                        break
+                    case 8: kind = 'sport'
+                        break
+                    default: kind = "clothes"
+                        break
+                 }
+                 getDetailKind(kind).then(res=>{
+                     this.goods.list = []
+                     this.goods.list.push(...res.data)
+                 })
+            }
+        },
+        created() {
+            getDetailKind('clothes').then(res=>{
+                this.goods.list = []
+                this.goods.list.push(...res.data)
+            })
+        }
     }
 </script>
 
 <style scoped>
-
+    #category {
+        height: 100vh;
+    }
+    .content {
+        position: absolute;
+        left: 100px;
+        top: 44px;
+        bottom: 49px;
+        z-index: 9;
+        overflow: hidden;
+    }
 </style>
